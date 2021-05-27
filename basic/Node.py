0745a13b2ee6a0cbded36fb942e32d638d932127
Michael-J98 <qianran_m_jia@163.com>
2021-05-27 19:47:27 +0800

    上传/Basic/Node.py, 修改/Basic/readme.txt

---
+++ basic/Node.py
@@ -0,0 +1,169 @@
+from random import seed
+from iota import AsyncIota, Seed, ProposedTransaction
+from iota.types import Address
+from typing import List
+import asyncio
+
+
+class Node():
+    """
+    Basic function of a Node including:
+        1. generate a new seed
+        2. generate a specific number(default 1) of new Tangle addresses
+        3. send a list of transactions(target address is included in transaction)
+        4. fetch message from a specific Tangle address
+
+    Important attributions:
+        self.addresses: list of Address, store all generated addresses
+        self.bundles: list of tail_transaction hashes in str type
+    Method Call Example:
+        # define event loop
+        loop = asyncio.get_event_loop()
+
+        # transfer coroutine object into task object
+        task = loop.create_task(coroutine object)
+
+        # trigger the task in event loop
+        loop.run_until_complete(task)
+    """
+    def __init__(self, **kwargs):
+        self.seed = None  # iota.Seed object
+        self.adapter = 'https://nodes.devnet.iota.org:443'  # url str
+        self.devnet = True  # Bool
+
+        self.addresses = []  # List[Address]
+        self.index = 0  # int
+        self.sent_tx_hashes = []  # list of Bundles
+        self.bundles = []  # list of Bundles
+        self.elapsed = 0
+        self.timeout = 120
+        self.polling_interval = 5
+
+        # overwrite if the attributions are provided
+        for key, value in kwargs.items():
+            exec('self.{}={}'.format(key, value))
+
+        # generate a specific seed for the instance
+        if self.seed is None:
+            self.seed_gen()
+
+        # construct a node connected to the Tangle
+        self.api = AsyncIota(
+            adapter=self.adapter,
+            seed=self.seed,
+            devnet=self.devnet,
+        )
+
+    def seed_gen(self):
+        self.seed = Seed.random()
+        assert self.seed is not None, 'fail to generate seed'
+
+    async def address_gen(self, count=1):
+        """generate one address from seed by default"""
+        response = await self.api.get_new_addresses(index=self.index, count=count)  # Dict
+        self.index += 1
+        adds = response['addresses']  # list of addresses
+        self.addresses.extend(adds)
+        return adds  # coroutine object[list of address]
+
+    async def send(self, transactions: List[ProposedTransaction]) -> bool:
+        """
+        Send a list of transactions as a bundle and monitor their confirmation
+        by the network.
+        """
+        print('Sending bundle...')
+        st_response = await self.api.send_transfer(transactions)
+        tx_hashes = [tx.hash for tx in st_response['bundle']]
+        self.sent_tx_hashes.extend(tx_hashes)
+        self.bundles.extend(tx_hashes)
+
+        print('Sent bundle with transactions: ')
+        print(*tx_hashes, sep='\n')
+
+        print('Checking confirmation...')
+        while len(self.sent_tx_hashes) > 0:
+            # Determine if transactions are confirmed
+            gis_response = await self.api.get_inclusion_states(self.sent_tx_hashes)
+
+            for i, (tx, is_confirmed) in enumerate(zip(self.sent_tx_hashes, gis_response['states'])):
+                if is_confirmed:
+                    print('Transaction %s is confirmed.' % tx)
+                    # No need to check for this any more
+                    del self.sent_tx_hashes[i]
+                    del gis_response['states'][i]
+
+            if len(self.sent_tx_hashes) > 0:
+                if self.timeout <= self.elapsed:
+                    # timeout reached, terminate checking
+                    return False  # coroutine object[bool]
+                # Show some progress on the screen
+                print('.')
+                # Put on hold for polling_interval. Non-blocking, so you can
+                # do other stuff in the meantime.
+                await asyncio.sleep(self.polling_interval)
+                self.elapsed = self.elapsed + self.polling_interval
+
+        # All transactions in the bundle are confirmed
+        return True  # coroutine object[bool]
+
+    async def fetch(self, address):
+        """
+        Argument:
+        address: Address | bundle hash
+
+        Return: unicode string(base64 encoded) | None
+        """
+        data = []
+
+        # if Address object is provided
+        if isinstance(address, Address):
+
+            response = await self.api.find_transaction_objects(addresses=[address])
+
+            if not response['transactions']:
+                print('Couldn\'t find data for the given address.')
+            else:
+                #print('Found:')
+                # Iterate over the fetched transaction objects
+                for tx in response['transactions']:
+                    # data is in the signature_message_fragment attribute
+                    # as trytes, we need to decode it into a unicode string
+                    found_data = tx.signature_message_fragment.decode(errors='ignore')
+                    data.append(found_data)
+
+        # if bundle hash object is provided
+        if isinstance(address, str):
+
+            if len(address) == 81:
+
+                # assume there is only one bundle under a tail_hash
+                response = await self.api.get_bundles([address])
+                for msg in response['bundles'][0]:
+                    data.append(msg.signature_message_fragment.decode(errors='ignore'))
+
+            else:
+                print('invaild bundle hash, the length of hash \
+                    should be 81, None will be returned')
+
+        # other types are invaild
+        if data == []:
+            print('Found No data or invaild address type:{}\n,\
+                should be iota.types.Address or str, None will be returned'.format(type(address)))
+
+        return data  # coroutine object[list of str]
+
+
+def decorator(coroutine):
+    """
+    a simple method to get return of a coroutine object
+    notice: this method is only for result display
+            cause it actually doesn't co-route with
+            other coroutine objects.[not sure]
+
+    """
+    # define event loop
+    loop = asyncio.get_event_loop()
+    # transfer coroutine object to task object
+    task = loop.create_task(coroutine)
+    # trigger task in event loop
+    return loop.run_until_complete(task)
--- basic/readme.txt
+++ basic/readme.txt
@@ -8,12 +8,57 @@
     解密数据
 
 3. Node.py
-功能
-    基本节点功能
-        生成seed
-        生成address
-        发送transaction
-        接收transaction
+
+********************************************************
+class Node(**kwargs)
+所有客户端节点的基础类（异步）
+	Parameters:
+		· seed - iota.Seed，用户种子, default = None
+		· adapter - str, URI string or BaseAdapter instance, default = 'https://nodes.devnet.iota.org:443'
+		· devnet - bool, 是否在测试网络上搭建, default = True
+		· addresses - List[iota.types.Address], 历史生成Tangle地址, default = []
+		· index - int, 种子生成地址的索引，每次生成地址会加一, default = 0
+		· sent_tx_hashes - List[iota.transaction.types.TransactionHash], 待发送transaction存放队列, default = []
+		· bundles - List[iota.transaction.types.TransactionHash], 历史transaction存放队列, default = []
+		· elapsed - int, 验证等待初始时间, default = 0
+		· timeout - int, 验证等待时间上限, default = 120
+		· polling_interval - int, 验证等待间隔, default = 5
+    ========
+    seed_gen()
+        生成新种子，并替换self.seed
+
+    ========
+    async address_gen(count)
+        根据种子在index=self.index处生成count个Tangle 地址, 并添加到self.addresses中
+        Parameters:
+                · count - int, 生成地址个数, default = 1
+        Return:
+                · adds - List[iot.types.Address], 本次调用生成的地址
+
+    ========
+    async send(transactions)
+        发送一系列transaction, tail_transaction hash将被存进self.bundles
+        从self.elapsed开始, 每隔self.polling_interval验证transactions是否被Tangle确认
+        Parameters:
+                · transactions - List[iota.transaction.creation.ProposedTransaction], 生成地址个数, default = 1
+        Return:
+                · state - bool, transaction是否全部被Tangle确认
+
+    ========
+    async fetch(address)
+        根据地址/哈希值查询transaction
+        Parameters:
+                · address - iota.types.Address | str, Tangle上地址类或者长度为81的tail_transaction哈希字符串
+        Return:
+                · state - List[str], 该地址上接收的所有信息, 可能被base64编码
+
+********************************************************
+def decorator(coroutine)
+一种简单的获得协程函数返回值的方法
+    Parameters:
+            · coroutine - coroutine object, 用async修饰的def在call时会返回coroutine object
+    Return:
+            · 不加async修饰的def的返回值
 
 4. Signature.py
 
